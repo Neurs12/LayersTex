88c3e98c8a1cc9d5a100c780205150ffebc512cb
Neurs <31346263+Neurs12@users.noreply.github.com>
2022-11-15 13:51:09 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "favicon.png": "66931295b77c5b72acdfed60168094f5",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "25e68df09de2de3251ea0175bded5c73",
-"/": "25e68df09de2de3251ea0175bded5c73",
-"main.dart.js": "aa6da42a5f63aaf12e7a56dbf6aeb2be",
+"index.html": "9768df363286532ec3ee60fe92191687",
+"/": "9768df363286532ec3ee60fe92191687",
+"main.dart.js": "47c8eddc343ba6b1e54b9aab2d837193",
 "manifest.json": "ceb08631e67f9c2dd7ac40f7a9ab2e8d",
 "version.json": "66dd901eafb484403f0b7b28de248234"
 };
